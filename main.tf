046c17eccbe95e55885ffcc241e99a2556480779
steveferns <steveferns@gmail.com>
2021-07-10 20:47:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.region
   assume_role {
-    role_arn = "arn:aws:iam::478131081250:role/terraform-test-role"
+    role_arn = "arn:aws:iam::478131081250:role/ecsInstanceRole"
   }
 }
 
